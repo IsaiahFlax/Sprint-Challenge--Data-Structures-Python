456f5c5f4484da1e9eec371d3dea386e4e0ff458
IsaiahFlax <flax.isaiah@gmail.com>
2020-07-31 19:17:48 -0600

    ring_buffer both appends and gets

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.tail = -1
+        self.head = 0
+        self.size = 0
 
     def append(self, item):
-        pass
+        if self.size == self.capacity:
+            self.current = item
+        else:
+            self.tail = (self.tail + 1) % self.capacity
+            self.queue[self.tail] = item
+            self.size +1
+            
 
     def get(self):
-        pass
+        a = []
+        for i in self.queue:
+            if i is not None:
+                a.append(i)
+        return a
